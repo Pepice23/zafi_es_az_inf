7aab354d744ceac55150857e86616d1e41bbc5b8
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-02 09:41:26 +0100

    Create CharacterCreator.vue

---
+++ src/views/CharacterCreator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NameInput @validCharacterName="getKarakterNev" />
+    <GenderSelect @genderSelected="getKarakterGender" />
+    <RaceClassPicker @factionRaceClass="getKarakterFRC" />
+
+    {{ karakterNev }} - {{ karakterGender }}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import GenderSelect from "@/components/GenderSelect.vue";
+import NameInput from "@/components/NameInput.vue";
+import RaceClassPicker from "@/components/RaceClassPicker.vue";
+
+export default defineComponent({
+  name: "CharacterCreator",
+  components: { RaceClassPicker, NameInput, GenderSelect },
+  data() {
+    return { karakterNev: "", karakterGender: "", karakterFRC: {} };
+  },
+  methods: {
+    getKarakterNev(nev) {
+      this.karakterNev = nev;
+    },
+    getKarakterGender(gender) {
+      this.karakterGender = gender;
+    },
+    getKarakterFRC(fRC) {
+      this.karakterFRC = fRC;
+    }
+  }
+});
+</script>
+
+<style scoped></style>
