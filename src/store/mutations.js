1bcb891911b3cc0231b5974623ba1025b18e8b29
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-02 13:36:45 +0100

    added mutations for new character

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,9 @@
 export const types = {
   SET_CHARACTER_NAME: "SET_CHARACTER_NAME",
+  SET_GENDER: "SET_GENDER",
+  SET_FACTION: "SET_FACTION",
+  SET_RACE: "SET_RACE",
+  SET_CLASS: "SET_CLASS",
   SET_EXISTING_CHARACTER_NAMES: "SET_EXISTING_CHARACTER_NAMES"
 };
 export default {
@@ -8,5 +12,17 @@ export default {
   },
   [types.SET_EXISTING_CHARACTER_NAMES](state, newCharacterNames) {
     state.meglevoKarakterNevek = newCharacterNames;
+  },
+  [types.SET_GENDER](state, gender) {
+    state.ujKarakter.karakterGender = gender;
+  },
+  [types.SET_FACTION](state, faction) {
+    state.ujKarakter.karakterGender = faction;
+  },
+  [types.SET_RACE](state, race) {
+    state.ujKarakter.karakterGender = race;
+  },
+  [types.SET_CLASS](state, kClass) {
+    state.ujKarakter.karakterGender = kClass;
   }
 };
