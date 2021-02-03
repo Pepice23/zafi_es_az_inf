cd4ccf013bdedaf3ee1a4a12e5d0abe79ab41ae1
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-03 10:35:01 +0100

    Create CharacterPanelPreview.vue

---
+++ src/components/CharacterPanelPreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="$store.state.ujKarakter.karakterPicture">
+    <img src="../assets/crests/Shaman.png" alt="crest" class="crest" />
+    {{ $store.state.ujKarakter.karakterPicture }}
+  </div></template
+>
+
+<script>
+export default {
+  name: "CharacterPanelPreview"
+};
+</script>
+
+<style scoped>
+.miniFlag {
+  width: 50px;
+  height: 50px;
+}
+.crest {
+  width: 200px;
+  height: 200px;
+}
+</style>
