e55dba7c504038e97a3803bb23cc816cd319b812
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-03 10:36:00 +0100

    added picture property

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,8 @@ export default createStore({
       karakterGender: "",
       karakterFaction: "",
       karakterRace: "",
-      karakterClass: ""
+      karakterClass: "",
+      karakterPicture: ""
     },
     meglevoKarakterNevek: []
   },
